10d53b6cea19ab4f1f465cdc4f258594823af028
ngocsuong <62738035+18521353@users.noreply.github.com>
2020-12-27 21:29:31 +0700

    Add files via upload

---
+++ Do_an_cuoi_ki/effect_mirror.py
@@ -0,0 +1,60 @@
+import cv2
+import numpy as np
+from PIL import Image
+
+def effect_mirror(img,option):
+  #get original image size
+  img_size = img.size
+
+  #create a new image
+  new_img = Image.new('RGB',(2*img_size[0], img_size[1]), (250,250,250))
+
+  #option = 0: horizontally
+  if option == 0:
+    #flip original image horizontally
+    hor_flippedImg = img.transpose(Image.FLIP_LEFT_RIGHT)
+
+    #merge 2 images
+    #mode 0: original image placed left
+    #mode 1: original image placed right
+    mode = int(input())
+    if mode == 0:
+      img1 = img
+      img2 = hor_flippedImg
+    else:
+      img1 = hor_flippedImg
+      img2 = img
+    new_img.paste(img1,(0,0))
+    new_img.paste(img2,(img_size[0],0))
+    return new_img
+
+  #option = 1: vertically 
+  else:
+    #flip original image vertically
+    ver_flippedImg = img.transpose(Image.FLIP_TOP_BOTTOM)
+
+    #merge 2 images
+    #mode 0: original image placed upper
+    #mode 1: original image placed lower
+    if mode == 0:
+      img1 = img
+      img2 = ver_flippedImg
+    else:
+      img1 = ver_flippedImg
+      img2 = img
+    new_img.paste(img1,(0,0))
+    new_img.paste(img2,(0,img_size[1]))
+    return new_img
+
+def main():
+  #read image
+  img_org = Image.open('./test.jpg')
+  cv2.imshow("Origin",img_org)
+  cv2.waitKey(0)
+
+  img_res = effect_mirror(img_org,0)
+  cv2.imshow("Result",img_res)
+  cv2.waitKey(0)
+
+if __name__ == "__main__":
+    main()
