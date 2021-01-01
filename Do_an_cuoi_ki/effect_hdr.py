c047060c42c7b67397ca267f26b9b65bbfe7ec53
Hoangthang017 <63088025+Hoangthang017@users.noreply.github.com>
2021-01-01 10:54:35 +0700

    Add files via upload

--- Do_an_cuoi_ki/effect_hdr.py
+++ Do_an_cuoi_ki/effect_hdr.py
@@ -1,49 +1,52 @@
-# import thư viện
+from PIL import Image, ImageEnhance
+from glob import glob
 import cv2
 import numpy as np
 
-# hàm tạo effect hdr 
-def effect_hdr(img,time= 15.0,gam= 1.3):
-    # Tạo ảnh hdr
-    merge_debevec = cv2.createMergeDebevec()
-    hdr_debevec = merge_debevec.process([img], times=np.array([time],dtype=np.float32))
-    # merge_robertson = cv2.createMergeRobertson()
-    # res_robertson = merge_robertson.process(img_original, times=time.copy())
-
-    # tạo tonemap
-    tonemap1 = cv2.createTonemap(gamma= gam)
-    res_debevec = tonemap1.process(hdr_debevec.copy())
-
-    # merge_mertens = cv2.createMergeMertens()
-    # res_mertens = merge_mertens.process(img_original)
+def effect_hdr(imgs):
+    # hợp nhất các ảnh phơi sáng bằng thuật toán Mertens
+    merge_mertens = cv2.createMergeMertens()
+    res_mertens = merge_mertens.process(imgs)
 
     # chuyển về định dạng chuẩn
-    res_debevec_8bit = np.clip(res_debevec*255, 0, 255).astype('uint8')
-    # res_robertson_8bit = np.clip(res_robertson*255, 0, 255).astype('uint8')
-    # res_mertens_8bit = np.clip(res_mertens*255, 0, 255).astype('uint8')
+    res_mertens_8bit = np.clip(res_mertens*255, 0, 255).astype('uint8')
+    return res_mertens_8bit
 
-    return res_debevec_8bit
+def hdr(img, rg = 1.8):
+    # tạo list ảnh và độ sáng của nó 
+    times = []
+    i = 0.5
+    while(i <= rg):
+        times.append(i)
+        i = i + 0.1
 
-# hàm chính
+    img_list = []
+    for time in times:
+        # điều chỉnh độ sáng của hình ảnh
+        result = ImageEnhance.Brightness(img).enhance(time)
+        img_temp = np.array(result)
+        img_list.append(img_temp)
+
+    # áp dụng effect 
+    res = effect_hdr(img_list)
+
+    # convert cv qua pillow
+    res = Image.fromarray(res)
+
+    return res 
+
 def main():
-    # đọc ảnh
-    img_ori = cv2.imread('./Test/test_2.jpg',1)
+    # đọc hình ảnh
+    img_ori = Image.open("./Test/test_1.jpg")
+    img_ori.show()
 
-    # show ảnh gốc
-    cv2.imshow('original',img_ori)
+    # khởi tạo hiệu ứng 
+    img_res = hdr(img_ori)
 
-    # ảnh gốc
-    cv2.imshow('original',img_ori)
+    # show kết quả
+    img_res.show()
 
-    # chỉnh sửa 
-    img_res = effect_hdr(img_ori)
-
-    cv2.imshow('debevec',img_res)
-    # cv2.imshow('robertson',res_robertson_8bit)
-    # cv2.imshow('mertens',res_mertens_8bit)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
-
-# hàm chính
 if __name__ == "__main__":
     main()
+
+
