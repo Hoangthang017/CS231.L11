672432851a97f8a1178a5528c6a937476875853c
Hoangthang017 <63088025+Hoangthang017@users.noreply.github.com>
2021-01-01 20:28:02 +0700

    Add files via upload

--- Do_an_cuoi_ki/ChannelShiftGlitch.py
+++ Do_an_cuoi_ki/ChannelShiftGlitch.py
@@ -1,29 +1,30 @@
 import cv2
-# import time
-# import random
-img = cv2.imread(r'girl.jpg')
-width = 500
-height = round(width / img.shape[1] * img.shape[0])
-print(height)
-img = cv2.resize(img,(width,height))
-print(img.shape)
-frame = img.copy()
-print(frame.shape)
-# n = 10
-# m = 20
-# start = time.time()
-# end = time.time()
-# elapsedtime = end - start
-# if elapsedtime > 0.4:
-#     n = random.randint(0,25)
-#     m = random.randint(0,25)
-#     start = time.time()
-n = 200
-m = 4
-frame[:frame.shape[0] - n,:frame.shape[1] - m,0] = img[n:,m:,0] 
-frame[:frame.shape[0] - m,:frame.shape[1] - n,1] = img[m:,n:,1] 
+from PIL import Image
+import numpy as np
 
-cv2.imshow('demo',frame)
-cv2.imwrite('ChannelShiftGlitch1.jpg',frame)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+def effect_glitch(img, n = 10, m = 20):
+    # chuyển sang dạng ma trận
+    img = np.array(img)
+    frame = img.copy()
+    # dịch chuyển kênh màu red sang NxM giá trị
+    frame[:frame.shape[0] - n,:frame.shape[1] - m,0] = img[n:,m:,0] 
+
+    # dịch chuyển kênh màu blue sang MxN giá trị
+    frame[:frame.shape[0] - m,:frame.shape[1] - n,1] = img[m:,n:,1] 
+
+    return frame
+
+def main():
+    # đọc hình ảnh
+    img_ori = Image.open("./Test/test_1.jpg")
+    img_ori.show()
+
+    # áp dụng effect
+    img_res = effect_glitch(img_ori)
+
+    # show kết quả
+    img_res = Image.fromarray(img_res)
+    img_res.show()
+
+if __name__ == "__main__":
+    main()
